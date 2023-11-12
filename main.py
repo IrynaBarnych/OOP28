59500df0c6ab47e3ca414c931ae4c7e06754e866
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-12 21:25:22 +0200

    додала код

--- main.py
+++ main.py
@@ -2,3 +2,21 @@
 # Створіть клас для конвертування з метричної системи в англійську, та навпаки.
 # Реалізуйте функціональність у вигляді статичних методів. Обов’язково реалізуйте конвертування
 # заходів довжини.
+
+class Converter:
+    @staticmethod
+    def meters_to_feet(meters):
+        return meters * 3.28084
+
+    @staticmethod
+    def feet_to_meters(feets):
+        return feets * 0.3048
+
+meters_length = 10
+feet_length = Converter.meters_to_feet(meters_length)
+print(f"{meters_length} метрів дорівнює {feet_length} футів")
+
+feet_length = 32.8084
+meters_length = Converter.feet_to_meters(feet_length)
+print(f"{feet_length} футів дорівнює {meters_length} метрів")
+
