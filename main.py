484f759b7a352493d960b59a7d5281b1706129df
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-12 21:58:18 +0200

    перевірила код, прибрала :

--- main.py
+++ main.py
@@ -24,5 +24,4 @@ celsius_temperature = Converter.cel_to_fah(celsius_temperature)
 
 print(f"{celsius_temperature} градусів Цельсія дорівнює {fahrenheit_temperature} градусів Фаренгейта")
 print(f"{fahrenheit_temperature} градусів Цельсія дорівнює {celsius_temperature} градусів Фаренгейта")
-
 print("Кількість підрахунків температури:", Converter.get_conversion_count())
