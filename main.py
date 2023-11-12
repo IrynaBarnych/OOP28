2fa77d649353d1f94e17cbf798f0b7c1f2e12fae
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-12 21:27:10 +0200

    оновила завдання

--- main.py
+++ main.py
@@ -1,43 +1,4 @@
 # Завдання 4
-# До вже реалізованого класу «Книга» додайте необхідні перевантажені методи та оператори.
+# Створіть клас InformationSystem, який має атрибут data - словник, де ключі - це імена користувачів, а значення -
+# список їх контактів. Реалізуйте методи класу для додавання нових користувачів та їх контактів.
 
-class Book:
-    count = 0
-    def __init__(self, title, author, genre, date):
-        self.title = title
-        self.author = author
-        self.genre = genre
-        self.date = date
-        Book.count += 1
-
-    def __sub__(self, other):
-        return self.date - other.date
-
-    def __lt__(self, other):
-        return self.date < other.date
-
-    def Book(self):
-        print(f"Назва книги: {self.title}, Автор: {self.author}, Жанр: {self.genre}, Дата випуску: {self.date}")
-
-    @staticmethod
-    def get_count():
-        return Book.count
-
-my_book_info1 = Book("Кобзар", "Т. Г. Шевченко", "різні літературні жанри", 1840)
-my_book_info1.Book()
-
-my_book_info2 = Book("Енеїда", "І. П. Котляревський", "комічна епічна поема", 1798)
-my_book_info2.Book()
-
-date_difference = my_book_info1 - my_book_info2
-print(f"Різниця в роках: {date_difference}")
-
-if my_book_info1 == my_book_info2:
-    print("Ці книги мають одного автора та однакову назву.")
-
-if my_book_info1 < my_book_info2:
-    print(f"{my_book_info1.title} вийшла раніше за {my_book_info2.title}.")
-else:
-    print(f"{my_book_info2.title} вийшла раніше за {my_book_info1.title}.")
-
-print("Кількість створених об'єктів:", Book.get_count())
