d9ca18a468f270e7c452deed1bae2b55c0ca960d
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-12 19:09:56 +0200

    додала код

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-#Завдання 5
-#Створіть клас Character, який має атрибути name, health та damage. Реалізуйте метод класу attack, який виводить
-#повідомлення про атаку гравця.
+#Завдання 6
+#Створіть клас Student, який має атрибути name, age, grade та courses. Реалізуйте метод класу add_course, який
+#додає новий предмет до списку курсів студента
 
-class Character:
-    def __init__(self, name, health, damage):
+class Student:
+    def __init__(self, name, age, grade):
         self.name = name
-        self.health = health
-        self.damage = damage
+        self.age = age
+        self.grade = grade
 
-    def attack(self, other):
-        print(f"{self.name} атакує і завдає {self.damage} одиниць урону {other.name}!")
 
-player1 = Character("Гравець 1", 100, 20)
-player2 = Character("Гравець 2", 90, 15)
+    def add_course(self, course):
+        print(f"Предмет {course} додано до списку курсів студента {self.name}.")
 
-player1.attack(player2)
-player2.attack(player1)
+
+student1 = Student("Іван", 20, "A")
+student2 = Student("Марія", 22, "B")
+
+student1.add_course("Математика")
+student1.add_course("Фізика")
+student2.add_course("Хімія")
 
 
 
