16583c61deb11137c1eb060a687c49ca371fa5b4
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-12 21:02:19 +0200

    оновила завдання

--- main.py
+++ main.py
@@ -1,45 +1,8 @@
-#До вже реалізованого класу «Дріб» додайте статичний метод, який при виклику повертає
-# кількість створених об’єктів класу «Дріб».
-class Fraction:
-    count = 0
-    def __init__(self, numerator, denominator):
-        self.numerator = numerator
-        self.denominator = denominator
-        Fraction.count += 1
-    def __str__(self):
-        return f"{self.numerator}/{self.denominator}"
-
-    def __add__(self, other):
-        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
-        new_denominator = self.denominator * other.denominator #спільний знаменник
-        return Fraction(new_numerator, new_denominator)
-
-    def __sub__(self, other):
-        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
-        new_denominator = self.denominator * other.denominator #спільний знаменник
-        return Fraction(new_numerator, new_denominator)
-
-    def __mul__(self, other):
-        new_numerator = self.numerator * other.numerator
-        new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
-
-    def __truediv__(self, other):
-        other.denominator, other.numerator = other.numerator, other.denominator
-        return Fraction.__mul__(self, other)
-
-    @staticmethod
-    def get_count():
-        return Fraction.count
-
-fraction1 = Fraction(4, 7)
-fraction2 = Fraction(5, 8)
-
-print(fraction1 + fraction2)
-print(fraction1 - fraction2)
-print(fraction1 * fraction2)
-print(fraction1 / fraction2)
-print("Кількість створених об'єктів:", Fraction.get_count())
+# Завдання 2
+# Створіть клас для конвертування температури з Цельсія
+# у Фаренгейт, і навпаки. У класі має знаходитися два статичні
+# методи: для конвертування з Цельсія у Фаренгейт і для конвертування з Фаренгейта у Цельсій.
+# Також клас має розрахувати кількість підрахунків температури та повернути це значення статичним методом.
 
 
 
