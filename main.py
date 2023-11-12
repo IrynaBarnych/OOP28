99b6f0d14eadd86826061822f3db28d8ebe030d8
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-12 18:37:03 +0200

    додала код

--- main.py
+++ main.py
@@ -1,43 +1,27 @@
-#Завдання 3
-#Створіть клас для підрахунку максимуму з чотирьох аргументів, мінімуму з чотирьох аргументів, середнє
-#арифметичне із чотирьох аргументів, факторіалу аргументу. Реалізуйте функціональність у вигляді статичних
-#методів.
+#Завдання 4
+#Створіть клас FileUtils, який має метод класу count_lines, який приймає шлях до файлу і повертає
+#кількість рядків у файлі.
 
-import math
-import math
+class FileUtils:
+    @classmethod
+    def count_lines(cls, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+                return len(lines)
+        except FileNotFoundError:
+            return -1
 
-class Calculator:
-    count_of_calculations = 0
+file_path = 'приклад.txt'
 
-    @staticmethod
-    def max_value(a, b, c, d):
-        Calculator.count_of_calculations += 1
-        return max(a, b, c, d)
 
-    @staticmethod
-    def min_value(a, b, c, d):
-        Calculator.count_of_calculations += 1
-        return min(a, b, c, d)
+lines_count = FileUtils.count_lines(file_path)
+print(f"Кількість рядків у файлі '{file_path}': {lines_count}")
 
-    @staticmethod
-    def average(a, b, c, d):
-        Calculator.count_of_calculations += 1
-        return (a + b + c + d) / 4
+print(f"Файл '{file_path}' не знайдено.")
 
-    @staticmethod
-    def factorial(n):
-        Calculator.count_of_calculations += 1
-        return math.factorial(n)
 
-a, b, c, d = 5, 10, 15, 20
 
-max_result = Calculator.max_value(a, b, c, d)
-min_result = Calculator.min_value(a, b, c, d)
-average_result = Calculator.average(a, b, c, d)
-factorial_result = Calculator.factorial(a)
 
-print("Максимум:", max_result)
-print("Мінімум:", min_result)
-print("Середнє арифметичне:", average_result)
-print(f"Факторіал {a}:", factorial_result)
-print("Кількість обчислень:", Calculator.count_of_calculations)
+
+
