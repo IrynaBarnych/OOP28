2b9bbc1289a2efb978bebc9f6dff26604b845f21
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-19 19:41:20 +0200

    додала статичний метод

--- main.py
+++ main.py
@@ -10,12 +10,27 @@ class Character:
 
     def attack(self, other):
         print(f"{self.name} атакує і завдає {self.damage} одиниць урону {other.name}!")
+        other.receive_damage(self.damage)
 
+    def receive_damage(self, damage):
+        self.health -= damage
+        print(f"{self.name} отримує {damage} одиниць урону. Залишок здоров'я: {self.health}")
+
+    @staticmethod
+    def display_info(player):
+        print(f"Інформація про {player.name}: Здоров'я - {player.health}, Урон - {player.damage}")
+
+# Створення гравців
 player1 = Character("Гравець 1", 100, 20)
 player2 = Character("Гравець 2", 90, 15)
+
+# Атаки гравців
 player1.attack(player2)
 player2.attack(player1)
 
+# Виведення інформації про гравців за допомогою статичного методу
+Character.display_info(player1)
+Character.display_info(player2)
 
 
 
